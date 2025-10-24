f4464a275f193f9622eaab5bae39c2657c71e3f0
lostvaynes <raditzengeance@gmail.com>
2025-10-24 23:24:08 +0700

    last1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+welcomeSpeech();
+
+function welcomeSpeech() {
+    /// Show prompt to ask for user's name
+    let name = prompt("Enter your name:");
+
+    // Greet the user with their name
+    document.getElementById('greet-name').innerHTML = `Hi ${name}, `;
+}
 // Sticky Navbar
 window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
